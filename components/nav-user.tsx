fbae4b8f4129124c60a6a33b45391a552c14cde2
Mainak Das <mainakdas104@gmail.com>
2026-07-21 15:46:44 +0530

    Integrate SignOutButton into NavUser component for improved user logout experience
    - Replaced the existing logout functionality with the SignOutButton from @clerk/nextjs, enhancing the authentication flow.
    - Maintained user information display within the dropdown menu, ensuring a seamless transition for users when logging out.

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -10,6 +10,7 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 import { useUser } from "@clerk/nextjs";
 import { LogOutIcon } from "lucide-react";
+import { SignOutButton } from "@clerk/nextjs";
 
 export function NavUser() {
   const { isLoaded, user } = useUser();
@@ -22,22 +23,24 @@ export function NavUser() {
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
-          <SidebarMenuButton size="lg" className="aria-expanded:bg-muted">
-            <Avatar>
-              <AvatarImage
-                src={user.imageUrl}
-                alt={user.fullName ?? "Default user"}
-              />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-medium">{user.fullName}</span>
-              <span className="truncate text-xs">
-                {user.emailAddresses[0].emailAddress}
-              </span>
-            </div>
-            <LogOutIcon className="ml-auto size-4" />
-          </SidebarMenuButton>
+          <SignOutButton>
+            <SidebarMenuButton size="lg" className="aria-expanded:bg-muted">
+              <Avatar>
+                <AvatarImage
+                  src={user.imageUrl}
+                  alt={user.fullName ?? "Default user"}
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.fullName}</span>
+                <span className="truncate text-xs">
+                  {user.emailAddresses[0].emailAddress}
+                </span>
+              </div>
+              <LogOutIcon className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </SignOutButton>
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
