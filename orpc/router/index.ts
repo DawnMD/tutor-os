9546ff0ec1b2031f3f097ac4af360f222559150f
Mainak Das <mainakdas104@gmail.com>
2026-07-21 15:45:47 +0530

    Add ownerClassRouter for class management functionality
    - Introduced ownerClassRouter with procedures for creating, archiving, unarchiving, and retrieving classes.
    - Enhanced the router structure by integrating the new class management features into the existing owner router, improving overall organization and functionality.

--- orpc/router/index.ts
+++ orpc/router/index.ts
@@ -1,11 +1,13 @@
 import { InferRouterInputs, InferRouterOutputs } from "@orpc/server";
 import { ownerBatchRouter } from "./owner/batch";
 import { ownerStudentRouter } from "./owner/student";
+import { ownerClassRouter } from "./owner/class";
 
 export const router = {
   owner: {
     batch: ownerBatchRouter,
     student: ownerStudentRouter,
+    class: ownerClassRouter,
   },
 };
 
---
+++ orpc/router/owner/class.ts
@@ -0,0 +1,60 @@
+import { ownerProcedure } from "@/orpc/orpc";
+import z from "zod";
+
+export const ownerClassRouter = {
+  createClass: ownerProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string().optional(),
+      }),
+    )
+    .handler(async ({ context, input }) => {
+      return await context.db.class.create({
+        data: {
+          name: input.name,
+          clerkOrganizationId: context.organizationId,
+          description: input.description,
+        },
+      });
+    }),
+  archieveClass: ownerProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .handler(async ({ context, input }) => {
+      return await context.db.class.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          archivedAt: new Date(),
+        },
+      });
+    }),
+  unArchieveClass: ownerProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .handler(async ({ context, input }) => {
+      return await context.db.class.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          archivedAt: null,
+        },
+      });
+    }),
+  getAllClass: ownerProcedure.handler(async ({ context }) => {
+    return await context.db.class.findMany({
+      where: {
+        clerkOrganizationId: context.organizationId,
+      },
+    });
+  }),
+};
