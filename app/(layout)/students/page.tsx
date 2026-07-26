c7179210cbd0bbaffab33276e3e9893a46f119ce
Mainak Das <mainakdas104@gmail.com>
2026-07-26 12:55:41 +0530

    Remove StudentsPage component and its associated imports
    - Deleted the StudentsPage component from the layout, which included authentication protection and rendering of the StudentTable. This change may be part of a larger refactor or redesign of the student management interface.

--- app/(layout)/students/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@clerk/nextjs/server";
-import { StudentTable } from "./_components/student-table";
-
-export default async function StudentsPage() {
-  await auth.protect();
-
-  return <StudentTable />;
-}
